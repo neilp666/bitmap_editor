f7a13c4fd1c6f9952ddb254627f644f62eca02ac
neil patel <neil@myindianweddingapp.com>
2018-08-20 19:14:46 +0100

    Updated error message if Bitmap grid dimensions out of bounds

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,3 +1,2 @@
 class Bitmap
-  
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -5,25 +5,25 @@ describe Bitmap do
     it 'should be invalid and raise error if width is less than 1' do
       expect do
         Bitmap.new('0', '5')
-      end.to raise_error(StandardError, 'Invalid Bitmap Grid')
+      end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
   end
 
     it 'should generate error if width is greater than 250' do
       expect do
         Bitmap.new('251', '5')
-      end.to raise_error(StandardError, 'Invalid Bitmap Grid')
+      end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
 
     it 'raises error if height is less than 1' do
       expect do
         Bitmap.new('5', '1')
-      end.to raise_error(StandardError, 'Invalid Bitmap Grid')
+      end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
 
     it 'raises error if height is greater than 250' do
       expect do
         Bitmap.new('5', '251')
-      end.to raise_error(StandardError, 'Invalid Bitmap Grid')
+      end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
 end
