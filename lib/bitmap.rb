75de4eaa94780038ebfc54dbd627efc8f1c6b411
neil patel <neil@myindianweddingapp.com>
2018-08-20 19:23:07 +0100

    Create pixels method using height and width and add to initialize method

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -2,5 +2,11 @@ class Bitmap
   def initialize(width, height)
     @width = width.to_i
     @height = height.to_i
+    @pixels = generate_pixels(@width, @height)
   end
+
+  def generate_pixels(width, height)
+    
+  end
+
 end
