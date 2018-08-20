06188087266e27705e6c454c58f020f10d74752d
neil patel <neil@myindianweddingapp.com>
2018-08-20 14:36:16 +0100

    Refactored code, created a new invlaid pixel string with new message. Also added validations to the pixel.rb & updated the pixel specs - all tests now pass in pixel_spec.rb

--- lib/pixel.rb
+++ lib/pixel.rb
@@ -1,10 +1,16 @@
 class Pixel
 
   attr_reader :colour
+  InvalidPixelError = Class.new(StandardError)
 
   def initialize
       @colour = 'O'
-      raise StandardError, 'Invalid Pixel'
+  end
+
+  def colour=(colour)
+    invalid_pixel_str = 'Pixel value must be a single Capital letter [A-Z]'
+    raise InvalidPixelError, invalid_pixel_str unless colour =~ /^[A-Z]$/
+    @colour = colour
   end
 
 end
