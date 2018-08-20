181c0a3fc8ed4bfa8842ae1488e3b7706f934513
neil patel <neil@myindianweddingapp.com>
2018-08-20 12:47:02 +0100

    Create pixel.rb file and create first test the default colour is 'O'

--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -2,4 +2,10 @@ require '../lib/pixel'
 
 RSpec.describe Pixel do
   let(:pixel) { Pixel.new }
+
+  describe 'initialize pixel colour' do
+    it 'should initialize with default colour `O`' do
+      expect(pixel.colour).to eq('O')
+    end
+  end
  end
