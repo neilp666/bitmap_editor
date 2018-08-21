68e35130b83c88df33c4c7c9d0c7c93745d43c0a
neil patel <neil@myindianweddingapp.com>
2018-08-21 10:57:33 +0100

    Updated specs to make hem shorter & readable

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -1,27 +1,27 @@
 require '../lib/bitmap.rb'
 
 describe Bitmap do
-  context 'Generating a new Bitmap' do
-    it 'should be invalid and raise error if width is less than 1' do
+  context '.new bitmap' do
+    it 'not valid if width is less than 1' do
       expect do
         Bitmap.new('0', '5')
       end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
   end
 
-    it 'should generate error if width is greater than 250' do
+    it 'not valid width is greater than 250' do
       expect do
         Bitmap.new('251', '5')
       end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
 
-    it 'raises error if height is less than 1' do
+    it 'not valid if height is less than 1' do
       expect do
         Bitmap.new('5', '0')
       end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
     end
 
-    it 'raises error if height is greater than 250' do
+    it 'not valid if height is greater than 250' do
       expect do
         Bitmap.new('5', '251')
       end.to raise_error(StandardError, 'Bitmap grid dimensions out of bounds')
