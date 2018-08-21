c5fd04830cb1b09a46c9af694bf16a6cdf247bc7
neil patel <neil@myindianweddingapp.com>
2018-08-21 09:34:38 +0100

    Create test on the initial setup of the bitmap grid using the commands I N M

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -2,4 +2,20 @@ require '../lib/command.rb'
 
 describe Command do
   let(:command) { Command.new(input) }
+
+  describe 'Initial setup of bitmap grid' do
+    context 'Vaild format I M N' do
+      let(:input) { 'I 6 6' }
+      it 'should not raise an invalid format error' do
+        should_not_raise_error
+      end
+    end
+
+      context 'Invalid format I X' do
+        let(:input) { 'I X' }
+        it 'should raise and invalid format error' do
+          expect { command }.to raise_error(Command::InvalidFormatError)
+        end
+      end
+   end
 end
