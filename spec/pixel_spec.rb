1e05ed59c725ed2c8d24acd1fa3b06d06fafc8ef
neil patel <neil@myindianweddingapp.com>
2018-08-21 11:08:40 +0100

    Updated  pixel specs to make it shorter & readable

--- spec/pixel_spec.rb
+++ spec/pixel_spec.rb
@@ -3,53 +3,53 @@ require '../lib/pixel'
 RSpec.describe Pixel do
   let(:pixel) { Pixel.new }
 
-  describe 'initialize pixel colour' do
-    it 'should initialize with default colour `O`' do
+  describe '.initialize pixel' do
+    it 'valid if default colour `O`' do
       expect(pixel.colour).to eq('O')
     end
   end
 
-  describe 'test the Pixel specifications' do
-    context 'should be a single captial letter from [A-Z] ' do
-      it 'should be capital' do
+  describe '.test pixel specifications' do
+    context 'when using single captial letter from [A-Z] ' do
+      it 'will be valid if capital' do
       expect(pixel.colour).to eq('O')
     end
   end
 
-    context  'use muliple capital letters for [A-Z]' do
-      it 'It should be invalid and raise error' do
+    context  'when using muliple capital letters from [A-Z]' do
+      it 'will be invalid & display error message to user ' do
         expect { pixel.colour = 'OO' }.to raise_error(Pixel::InvalidPixelError)
       end
     end
 
-    context 'use multiple single small letters from [a-z]' do
-      it 'should be invalid and raise error' do
+    context 'when using multiple single small letters from [a-z]' do
+      it 'will be invalid & display error message to user ' do
         expect { pixel.colour = 'o'}.to raise_error(Pixel::InvalidPixelError)
       end
     end
 
-    context 'use multiple small letters from [a-z]' do
-      it 'should be invalid and raise error' do
+    context 'when using multiple small letters from [a-z]' do
+      it 'will be invalid & display error message to user' do
         expect { pixel.colour = 'OO'}.to raise_error(Pixel::InvalidPixelError)
       end
     end
 
-    context 'using multiple letters from [A-Za-z]' do
-      it 'should be invalid and raise error' do
+    context 'when using using multiple letters from [A-Za-z]' do
+      it 'will be invalid & display error message to user' do
         expect { pixel.colour = 'Oo'}.to raise_error(Pixel::InvalidPixelError)
       end
     end
 
-    context 'using multiple letters from [A-Za-z0-9]' do
-      it 'should be invalid and raise error' do
+    context 'when using multiple letters from [A-Za-z0-9]' do
+      it 'will be invalid & display error message to user' do
        expect { pixel.colour = 'Oo0' }.to raise_error(Pixel::InvalidPixelError)
        expect { pixel.colour = 'O0' }.to raise_error(Pixel::InvalidPixelError)
        expect { pixel.colour = 'o0' }.to raise_error(Pixel::InvalidPixelError)
      end
    end
 
-    context 'using a single digit' do
-      it 'should be invalid and raise error' do
+    context 'when using a single digit' do
+      it 'will be invalid & display error message to user' do
         expect { pixel.colour = '1'}.to raise_error(Pixel::InvalidPixelError)
       end
     end
