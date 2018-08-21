f228f85281eb128613fec166e773e824d651fb0f
neil patel <neil@myindianweddingapp.com>
2018-08-21 09:59:43 +0100

    Added tests to make sure the supported commands are valid

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -6,15 +6,36 @@ describe Command do
   describe 'Initial setup of bitmap grid' do
     context 'Vaild format I M N' do
       let(:input) { 'I 6 6' }
-      it 'should not raise an invalid format error' do
+      it 'should not raise error' do
         should_not_raise_error
       end
     end
 
       context 'Invalid format I X' do
         let(:input) { 'I X' }
-        it 'should raise and invalid format error' do
+        it 'should raise error' do
           expect { command }.to raise_error(Command::InvalidFormatError)
+        end
+      end
+
+      context '`C`' do
+        let(:input) { 'C' }
+        it 'should not raise error' do
+          should_not_raise_error
+        end
+      end
+
+      context '`S`' do
+        let(:input) { 'S'}
+        it 'should not raise error' do
+          should_not_raise_error
+        end
+      end
+
+      context '`L X Y C`' do
+        let(:input) { 'L 2 3 C' }
+        it 'should not raise error' do
+          should_not_raise_error
         end
       end
    end
