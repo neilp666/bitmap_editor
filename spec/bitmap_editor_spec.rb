e3e6855ad999fe24935c58e4f27e92ee36a87dff
neil patel <neil@myindianweddingapp.com>
2018-08-22 10:49:10 +0100

    Create test to raise error if C,S,V and H command are used without generating a bitmap

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -10,4 +10,22 @@ describe BitmapEditor do
       expect { @bitmap_editor.execute(['L','1','2','C']) }.to raise_error(StandardError, 'No bitmap found')
     end
   end
+
+    it 'raises error when running the C command' do
+      expect { @bitmap_editor.execute(['C']) }.to raise_error(StandardError, 'No bitmap found')
+    end
+
+
+    it 'raises error when running the S command' do
+      expect { @bitmap_editor.execute(['S']) }.to raise_error(StandardError, 'No bitmap found')
+    end
+
+
+    it 'raises error when running the V command' do
+      expect { @bitmap_editor.execute(['V', '1', '2', '3','O']) }.to raise_error(StandardError, 'No bitmap found')
+    end
+
+    it 'raises error when running the H command'  do
+      expect { @bitmap_editor.execute(['H', '1', '2', '3', 'O']) }.to raise_error(StandardError, 'No bitmap found')
+    end
 end
