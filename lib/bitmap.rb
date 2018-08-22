6cd4e4c1548b68bf23e19047adcb8937bd824fde
neil patel <neil@myindianweddingapp.com>
2018-08-22 16:56:37 +0100

    Create show and clear methods to display and reset the bitmap

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,6 +1,6 @@
 class Bitmap
    attr_reader :bitmap
-   
+
   def initialize(width, height)
     @width = width.to_i
     @height = height.to_i
@@ -11,6 +11,20 @@ class Bitmap
     if is_bitmap_out_of_bounds?(width) || is_bitmap_out_of_bounds?(height)
       raise StandardError, 'Bitmap grid dimensions out of bounds'
     end
+  end
+
+  def show!
+    return if @bitmap.nil?
+    str = ""
+    @bitmap.to_a.each do |row|
+      str += row.join("")
+      str += "\n"
+    end
+    puts str
+  end
+
+  def clear!
+    return if @bitmap.nil?
   end
 
   private
