6fb9a3709379ce0966fcf169457bf86ac4b2fb1a
neil patel <neil@myindianweddingapp.com>
2018-08-22 10:28:54 +0100

    Create test to raise error if no bitmap generated

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,4 +4,10 @@ describe BitmapEditor do
   before(:example) do
     @bitmap_editor = BitmapEditor.new
   end
+
+  context 'when no bitmap has been generated' do
+    it 'raises error when running the L command' do
+      expect { @bitmap_editor.execute(['L','1','2','C']) }.to raise_error(StandardError, 'No bitmap found')
+    end
+  end
 end
